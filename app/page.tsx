3f045dc1a21c4a04207feb9afc0b6b319c05bc72
amitberko <amit.berko10@gmail.com>
2025-08-19 18:59:04 +0300

    Changed page name

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 import ActionsBar from './components/ActionsBar'
 import TaskList from './components/TaskList'
 
-export default function TodosPage() {
+export default function TasksPage() {
 	return (
 		<Container maxWidth="md">
 			<Stack rowGap={3} sx={{ mt: { xs: 1.5, sm: 4 }, alignItems: 'center' }}>
